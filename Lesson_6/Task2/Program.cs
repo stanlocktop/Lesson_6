cc85b496c98cb67cf493b39ba2622ef621885e47
stanlocktop <wlad-test@ukr.net>
2023-09-16 18:50:26 +0300

    finish task 2

--- Lesson_6/Task2/Program.cs
+++ Lesson_6/Task2/Program.cs
@@ -4,8 +4,25 @@ class Program
 {
     static void Main()
     {
-      
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Console.ForegroundColor = ConsoleColor.Yellow; // устанавливаем цвет
+        const int maxNumber = 10;
+        int[] A = new int[maxNumber];
+        int number;
+        int i;
+        int result;
+        for (i = 0; i < maxNumber; i++)
+        {
+            A[i] = i;
+        }
+        number = 5;
+        Console.WriteLine("Задане число: " + number);
+        result = 1;
+        for (i = 0; i < maxNumber; i++)
+            if (A[i] > number)
+                result = result * A[i];
+        Console.WriteLine("Результат добутку елементів масиву, які більше за задане число: " + result);
+        Console.ResetColor(); // сбрасываем в стандартный
         Console.ReadKey();
     }
 }
-   
