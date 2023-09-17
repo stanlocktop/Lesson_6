128308308f9b4974da04cdf601786312697f030a
stanlocktop <wlad-test@ukr.net>
2023-09-17 15:28:58 +0300

    test 3 task

--- Lesson_6/Task3/Program.cs
+++ Lesson_6/Task3/Program.cs
@@ -1,12 +1,12 @@
 ﻿
-// 2) Знайти добуток елементів масиву, які більші за задане число.
+// 3) Задано квадратну матрицю цілих чисел. Підрахуйте кількість негативних та позитивних елементів.
 class Program
 {
     static void Main()
     {
         Console.OutputEncoding = System.Text.Encoding.Unicode;
         Console.ForegroundColor = ConsoleColor.Yellow; // устанавливаем цвет
-       
+        Console.WriteLine("test");
         Console.ResetColor(); // сбрасываем в стандартный
         Console.ReadKey();
     }
