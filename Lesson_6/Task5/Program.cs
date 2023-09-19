b5fecc0ce027c9558d9d7cb87bd388147a28e585
stanlocktop <wlad-test@ukr.net>
2023-09-19 20:01:13 +0300

    update task5 - menu

--- Lesson_6/Task5/Program.cs
+++ Lesson_6/Task5/Program.cs
@@ -1,33 +1,99 @@
 ﻿using System;
-// Створіть програму, яка дозволяє вводити оцінки студента з різних предметів та обчислювати середню оцінку. Використовуйте масиви для зберігання оцінок. 
-// Завдання повинно включати такі етапи:
+using System.Collections.Generic;
 
-//- Введення оцінок студента з різних предметів (наприклад, математика, історія, фізика).
-//- Збереження цих оцінок у масивах.
-//- Обчислення середньої оцінки студента на основі введених даних.
-//- Виведення середньої оцінки на екран.
-//- Масиви використовуються для зберігання оцінок студента, і програма має виводити результат на консоль.
-class Program
+namespace StudentGradeCalculator
 {
-    static void Main()
+    class Program
     {
-        Console.OutputEncoding = System.Text.Encoding.Unicode;
-        int numberOfSubjects = 4;
-        double[] grades = new double[numberOfSubjects];
-        string[] subjects = { "Математика", "Історія", "Англійська мова", "Інформатика" };
-        for (int i = 0; i < numberOfSubjects; i++)
+        static void Main(string[] args)
         {
-            Console.Write($"Введіть оцінку студента Пирогов з предмету - {subjects[i]}: ");
-            grades[i] = double.Parse(Console.ReadLine());
+            Dictionary<string, double> grades = new Dictionary<string, double>();
+            int choice;
+
+            do
+            {
+                Console.WriteLine("Оберіть опцію:");
+                Console.WriteLine("1. Ввести оцінки");
+                Console.WriteLine("2. Переглянути введені оцінки");
+                Console.WriteLine("3. Обчислити середню оцінку");
+                Console.WriteLine("4. Вийти");
+                Console.Write("Ваш вибір: ");
+
+                while (!int.TryParse(Console.ReadLine(), out choice))
+                {
+                    Console.WriteLine("Некоректний ввід. Будь ласка, введіть число від 1 до 4.");
+                }
+
+                switch (choice)
+                {
+                    case 1:
+                        InputGrades(grades);
+                        break;
+                    case 2:
+                        DisplayGrades(grades);
+                        break;
+                    case 3:
+                        CalculateAverage(grades);
+                        break;
+                    case 4:
+                        Console.WriteLine("Дякуємо за використання програми!");
+                        break;
+                    default:
+                        Console.WriteLine("Некоректний вибір. Будь ласка, оберіть опцію від 1 до 4.");
+                        break;
+                }
+
+            } while (choice != 4);
         }
-        //average
-        double sum = 0;
-        for (int i = 0; i < numberOfSubjects; i++)
+
+        static void InputGrades(Dictionary<string, double> grades)
         {
-            sum += grades[i];
+            string[] subjects = { "Математика", "Історія", "Інформатика", "Англійська мова" };
+
+            foreach (string subject in subjects)
+            {
+                Console.Write($"Введіть оцінку для предмету '{subject}': ");
+                while (true)
+                {
+                    if (double.TryParse(Console.ReadLine(), out double grade) && grade >= 0 && grade <= 100)
+                    {
+                        grades[subject] = grade;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Некоректний ввід. Будь ласка, введіть дійсне число від 0 до 100.");
+                        Console.Write($"Введіть оцінку для предмету '{subject}': ");
+                    }
+                }
+            }
         }
-        double averageGrade = sum / numberOfSubjects;
-        Console.WriteLine($"Середня оцінка студента Пирогов: {averageGrade:F2}");
-        Console.ReadKey();
+
+        static void DisplayGrades(Dictionary<string, double> grades)
+        {
+            Console.WriteLine("Введені оцінки:");
+            foreach (var kvp in grades)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+            }
+        }
+
+        static void CalculateAverage(Dictionary<string, double> grades)
+        {
+            if (grades.Count == 0)
+            {
+                Console.WriteLine("Немає введених оцінок для обчислення середньої оцінки.");
+                return;
+            }
+
+            double sum = 0;
+            foreach (var kvp in grades)
+            {
+                sum += kvp.Value;
+            }
+            double averageGrade = sum / grades.Count;
+
+            Console.WriteLine($"Середня оцінка: {averageGrade:F2}");
+        }
     }
 }
